fe06ff8c3d673ba59bec4df73e1de0992ceb85c0
sachin <sachinkumar854881@gmail.com>
2024-03-09 00:06:10 +0530

    add files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{useReducer} from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// initialize intial state
+const initialVal = 0;
+
+// reducer function is define its call by dispatch function and dispatch call when use click btn
+const reducer = (state,action) =>{
+    switch(action.type){
+        case'incre':
+            return state+1;
+        case 'decre':
+            return state-1;
+        default:
+            return state
+    }
+}
+
+function App(){
+    const [count, dispatch] = useReducer(reducer, initialVal) //destructuring 
+
+    // increment function calll when user click btn
+    const increReducer = () =>{
+        dispatch({type:'incre'})
+    }
+    // decrement function calll when user click btn
+    const decreReducer = () =>{
+        dispatch({type:'decre'})
+    }
+    return(
+        <div>
+            <center>
+            <h1>Counter:{count}</h1>
+            <button onClick={increReducer}>increment</button>
+            <button onClick={decreReducer}>decrement</button>
+            </center>
+        </div>
+    )
 }
 
 export default App;
